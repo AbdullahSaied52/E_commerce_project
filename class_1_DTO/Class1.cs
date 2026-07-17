7fa45af0d921d602cc454d66fb8aabb7a3cf470b
AbdullahSaied52 <a9407602@gmail.com>
2026-07-17 09:07:08 +0300

    add a DTO class

---
+++ class_1_DTO/Class1.cs
@@ -0,0 +1,20 @@
+﻿namespace class_1_DTO
+{
+    public class product_dto
+    {
+        public int id { get; set; }
+        public string type { get; set; }
+        public string name { get; set; }
+        public int amount { get; set; }
+        public int price { get; set; }
+
+        public product_dto(int id, string name, string type, int amount, int price)
+        {
+            this.id = id;
+            this.name = name;
+            this.type = type;
+            this.price = price;
+            this.amount = amount;
+        }
+    }
+}
