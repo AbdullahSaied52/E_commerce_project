e46477266f090082b1f9747532cc6d6f318a4c93
AbdullahSaied52 <a9407602@gmail.com>
2026-07-17 09:06:22 +0300

    add bussiness logic layer

---
+++ BLL/Class1.cs
@@ -0,0 +1,23 @@
+﻿using class_1_DTO;
+using dal_4;
+namespace BLL
+{
+    public class ClsBLL
+    {
+        public static void insert(product_dto p)
+        {
+            dal.insert(p);
+        }
+
+        public static product_dto info(string name)
+        {
+            return dal.list_info(name);
+        }
+
+        public static int sell(string name,int amount)
+        {
+            return dal.sell(name, amount);
+        }
+
+    }
+}
